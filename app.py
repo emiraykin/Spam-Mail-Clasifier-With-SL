c71fbdeec07390f9127d738b24ef5b8af205a96d
bbakim <baris.bakim@gmail.com>
2023-12-18 13:36:26 +0300

    add&ref: db knn buttons added - button names changed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from logistic_regression import lr_predict_spam
 from naive import naive_predict_spam
 from flask_cors import CORS
 from flask import Flask, render_template, request, flash
+from knn import knn_predict_spam
 
 classifierApp = Flask(__name__, static_url_path='/static')
 CORS(classifierApp)
@@ -19,7 +20,7 @@ def lr_controller():
     flash(f"This email is {result}")
     return render_template("index.html", test_acc=test_acc, train_acc=train_acc)
 
-@classifierApp.route("/mailController_2", methods=['POST', 'GET'])
+@classifierApp.route("/mailController_nb", methods=['POST', 'GET'])
 def naive_controller_2():
     result, test_acc, train_acc = naive_predict_spam([str(request.form["mail"])])
     flash(f"This email is {result}")
@@ -30,6 +31,22 @@ def mail_controller_3():
     #result, test_acc, train_acc = naive_predict_spam([str(request.form["mail"])])
     flash(f"This email is a {result} email")
     return render_template("index.html", test_acc=test_acc, train_acc=train_acc)
+
+@classifierApp.route("/mailController_knn", methods=['POST', 'GET'])
+def knn_controller():
+    result, test_acc, train_acc = knn_predict_spam([str(request.form["mail"])])
+    flash(f"This email is {result}")
+    return render_template("index.html", test_acc=test_acc, train_acc=train_acc)
+
+@classifierApp.route("/databaseController_1", methods=['POST', 'GET'])
+def import_db_1():
+    flash("database 1 will be imported here")
+    return render_template("index.html", test_acc=0, train_acc=0)
+
+@classifierApp.route("/databaseController_2", methods=['POST', 'GET'])
+def import_db_2():
+    flash("database 2 will be imported here")
+    return render_template("index.html", test_acc=0, train_acc=0)
 
 if __name__ == '__main__':
     classifierApp.run()
